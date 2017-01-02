f1be32705dab7bb87b416aa454dc00ebeb762acc
David Bal <david.bal37@gmail.com>
2017-01-01 23:17:23 -0800

    added interval control

--- WebContent/rrule.js
+++ WebContent/rrule.js
@@ -13,7 +13,7 @@ function loadXMLDoc()
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4) {
 			if (xmlhttp.status = 200){
-				document.getElementById("message").innerHTML = xmlhttp.responseText;
+				document.getElementById("result").innerHTML = xmlhttp.responseText;
 				} else
 				{
 					alert("Action can't be performed");
@@ -27,4 +27,51 @@ function loadXMLDoc()
     var dateTimeStart = document.getElementById('dateTimeStart').value
     var limit = document.getElementById('limit').value
 	xmlhttp.send("rruleContent=" + rruleContent + "&dateTimeStart=" + dateTimeStart + "&limit=" + limit);
+}
+
+function buildRRule()
+{
+    var freq = document.getElementById('freq').value;
+    var rrule = "RRULE:FREQ=" + freq;
+
+    var interval = document.getElementById('interval').value;
+    if (interval > 1)
+	{
+    	rrule += ";INTERVAL=" + interval;
+	}
+
+    document.getElementById('rruleContent').value = rrule;
+    
+    // Interval type word
+    var intervalType;
+    if (freq === "DAILY")
+	{
+    	intervalType = "day";
+	} else if (freq === "WEEKLY")
+	{
+		intervalType = "week";
+	} else if (freq === "MONTHLY")
+	{
+		intervalType = "month";
+	} else if (freq === "YEARLY")
+	{
+		intervalType = "year";
+	} else if (freq === "SECONDLY")
+	{
+		intervalType = "second";
+	} else if (freq === "MINUTELY")
+	{
+		intervalType = "minute";
+	} else if (freq === "HOURLY")
+	{
+		intervalType = "hour";
+	}
+
+    var interval = document.getElementById('interval').value;
+    if (interval > 1)
+	{
+    	intervalType += "s";
+	}
+    document.getElementById('intervalType').innerHTML = intervalType;
+    console.log(intervalType);
 }
